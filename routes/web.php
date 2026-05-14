88c00498b1d43fcf0830255c6b4a17093577dfb8
kalpeshadhvaryu <84937819+kalpeshadhvaryu@users.noreply.github.com>
2026-05-14 13:44:47 +0530

    feat: description of backend changes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $webDashboardUrl = rtrim((string) env('WEB_DASHBOARD_URL', ''), '/');
-
-    if ($webDashboardUrl !== '') {
-        return redirect()->away($webDashboardUrl.'/login');
-    }
-
-    return view('welcome');
+    return redirect('/web_dashboard');
 });
